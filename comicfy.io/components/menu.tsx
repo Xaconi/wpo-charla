8262e05025d2673096f536d111358eaab226140a
Nicolás Joel Giacconi Fernández <xaco.89@gmail.com>
2022-05-11 00:04:09 +0200

    Fix list item key

--- comicfy.io/components/menu.tsx
+++ comicfy.io/components/menu.tsx
@@ -7,8 +7,8 @@ import styles from '../styles/Menu.module.scss'
 const Menu = () => {
     return (
         <nav className={`${styles.csMenu} cs-flex cs-flex--row cs-flex--center`}>
-            {menuData.map((menuItem: any) => {
-                return <a className={`${styles.csMenu__link} cs-text-type1`} href="">{menuItem.title}</a>
+            {menuData.map((menuItem: any, index: number) => {
+                return <a key={index} className={`${styles.csMenu__link} cs-text-type1`} href="">{menuItem.title}</a>
             })}
         </nav>
     )
