71f771ced2c6372c26c8a576fdf7c624c44062a3
Nicolás Joel Giacconi Fernández <xaco.89@gmail.com>
2022-05-12 23:17:00 +0200

    Add copyright

--- comicfy.io/components/footer.tsx
+++ comicfy.io/components/footer.tsx
@@ -10,27 +10,32 @@ import { Newsletter } from './newsletter'
 
 const Footer = () => {
     return(
-        <footer className={`${styles.csFooter} cs-flex cs-flex--row cs-flex--center-top cs-flex--gap cs-flex--wrap`}>
-            { menuFooterData.map((footerMenu: FooterMenuItem) => {
-                return (
-                    <>
-                        <div className={`${styles.csFooterItem}`}>
-                            <h4 className={`${styles.csFooterItem__title} cs-text-type1`}>{ footerMenu.title }</h4>
-                            <ul key={footerMenu.id}>
-                                { footerMenu.items?.map((footerMenuItem: FooterMenuItem) => {
-                                    return (
-                                        <li className={`${styles.csFooterItem__item} cs-text-type2`}>
-                                            <a href="/">{ footerMenuItem.title }</a>
-                                        </li>
-                                    )
-                                })}
-                            </ul>
-                        </div>
-                    </>
-                )
-            })}
-            <Newsletter></Newsletter>
-        </footer>
+        <>
+            <footer className={`${styles.csFooter} cs-flex cs-flex--row cs-flex--center-top cs-flex--gap cs-flex--wrap`}>
+                { menuFooterData.map((footerMenu: FooterMenuItem) => {
+                    return (
+                        <>
+                            <div className={`${styles.csFooterItem}`}>
+                                <h4 className={`${styles.csFooterItem__title} cs-text-type1`}>{ footerMenu.title }</h4>
+                                <ul key={footerMenu.id}>
+                                    { footerMenu.items?.map((footerMenuItem: FooterMenuItem) => {
+                                        return (
+                                            <li className={`${styles.csFooterItem__item} cs-text-type2`}>
+                                                <a href="/">{ footerMenuItem.title }</a>
+                                            </li>
+                                        )
+                                    })}
+                                </ul>
+                            </div>
+                        </>
+                    )
+                })}
+                <Newsletter></Newsletter>
+            </footer>
+            <section className={styles.csCopyright}>
+                Copyright © 2022 Comicfy.io. All rights reserved.
+            </section>
+        </>
     )
 }
 
